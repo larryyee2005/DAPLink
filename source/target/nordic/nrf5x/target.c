af16b1713e236cd4242714179cc2d6bbd2a7e385
jixing.zou <jixing@redbear.cc>
2017-01-06 16:19:11 +0800

    Bugfixes : Update target_device, should use nrf52832's device_target.

--- source/target/nordic/nrf5x/target.c
+++ source/target/nordic/nrf5x/target.c
@@ -25,7 +25,7 @@
 #include "flash_blob.c"
 
 // target information
-target_cfg_t target_device = {  // Default is nRF51
+target_cfg_t target_device_nrf51 = {  // Default is nRF51
     .sector_size    = 1024,
     .sector_cnt     = (KB(256) / 1024),
     .flash_start    = 0,
@@ -36,13 +36,13 @@ target_cfg_t target_device = {  // Default is nRF51
     .erase_reset    = 1,
 };
 
-target_cfg_t target_device_nrf52 = {
+target_cfg_t target_device = {
     .sector_size    = 4096,
     .sector_cnt     = (KB(512) / 1024),
     .flash_start    = 0,
     .flash_end      = KB(512),
     .ram_start      = 0x20000000,
-    .ram_end        = 0x20008000,
+    .ram_end        = 0x20010000,
     .flash_algo     = (program_target_t *) &flash_nrf52,
     .erase_reset    = 1,
 };
